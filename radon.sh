8c02e30191283bb07a872fca4beb2003f5f7acff
jddayley <jddayley@gmail.com>
2021-12-15 10:51:14 -0500

    Update radon.sh
    Remove credentials

--- radon.sh
+++ radon.sh
@@ -5,6 +5,6 @@ while [ $s -le 5 ]
 do
   echo "# of Radon $x samples"
   x=$(( $x + 1 ))
-  python3 radon_meter.py -v -a C2:7B:7C:49:CB:06 -m -ma -ms 192.168.0.116 -mp 1883 -mu jddayley -mw java
+  python3 radon_meter.py -v -a C2:7B:7C:49:CB:06 -m -ma -ms  -mp  -mu  -mw 
   sleep 300
 done
